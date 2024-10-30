230f674c6ed04a7d89e747bd786ce940b09abb0b
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-30 18:56:46 -0500

    Add action to create conversations

--- app/layouts/chat.tsx
+++ app/layouts/chat.tsx
@@ -11,8 +11,10 @@ import {
 	SidebarProvider,
 } from "app:components/ui/sidebar";
 import { authenticate } from "app:helpers/auth";
+import { rateLimit } from "app:helpers/rate-limit";
 import { ok } from "app:helpers/response";
 import { ConversationsRepository } from "app:repositories.server/conversations";
+import { initiateConversation } from "app:services.server/initiate-conversation";
 import type * as Route from "types:layouts/+types.chat";
 import { Form, NavLink, Outlet } from "react-router";
 
@@ -25,6 +27,13 @@ export async function loader({ request }: Route.LoaderArgs) {
 			return { id: c.id, name: c.name, path: c.path };
 		}),
 	});
+}
+
+export async function action({ request }: Route.ActionArgs) {
+	await rateLimit(request.headers);
+	let user = await authenticate(request);
+	let conversation = await initiateConversation({ user });
+	throw redirect(conversation.path);
 }
 
 export default function Component({ loaderData }: Route.ComponentProps) {
